2195f8396e3230f845e6cbae3b63dc4df7a025e0
MohammedRiyazdeen <ariyazdeen128@gmail.com>
2025-08-05 19:20:52 +0530

    modified the views in post app

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 # accounts/views.py
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login, logout
 from .forms import SignupForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
